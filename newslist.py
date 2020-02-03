67633e9671cea064704aded8275bf3ba04a8806d
Trish Panda <annarslowik@gmail.com>
2020-02-02 20:23:53 -0500

    grab DNA headlines from search page.

--- newslist.py
+++ newslist.py
@@ -1,17 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
 
-page = requests.get('https://www.nature.com/nature/articles')
+page = requests.get('https://www.nature.com/search?q=DNA&date_range=last_7_days')
 
 soup = BeautifulSoup(page.content, features="lxml")
 
-articles = soup.findAll("article")
+headlines = soup.findAll("h2", {"itemprop": "headline"})
 
 data = []
-for article in articles:
-    time = article.find("time").getText()
-    headline = article.find("a", {"data-track-label" : "link"}).getText().splitlines()[2]
-    data.append([time, headline])
-    print(time + ": " + headline)
+for headline in headlines:
+    print(headline.getText().strip() + "\n")
 
     
